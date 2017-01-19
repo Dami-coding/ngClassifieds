f6de07d30e9a90c8120069b85593d89a725bc9c5
dami <boldsdee@aol.com>
2017-01-19 12:11:33 +0000

    An angular ecommerce app

---
+++ scripts/app.js
@@ -0,0 +1,15 @@
+angular
+  .module("ngClassifieds", ["ngMaterial"])
+  .config(function($mdThemingProvider){
+
+    $mdThemingProvider.theme('default')
+      .primaryPalette('pink')
+      .accentPalette('orange');
+
+  })
+   .directive("helloWorld", function(){
+    return {
+      template: "<h1>{{ message }}</h1>"
+    }
+   });
+
