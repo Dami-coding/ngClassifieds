3e471e972b23a03d9939444143ddfa7d0b5150d3
dami <boldsdee@aol.com>
2017-06-02 16:27:37 +0100

    Added contacts for new users

--- components/classifieds.ctr.js
+++ components/classifieds.ctr.js
@@ -10,6 +10,12 @@
        $scope.classifieds = classifieds.data;
       });
 
+      var contact = {
+        name: "Dami Osinaike",
+        phone: "(079) 513 37439",
+        email: "boldsdee@gmail.com"
+      }
+
       $scope.openSidebar = function(){
         $mdSidenav('left').open();
       }
@@ -20,6 +26,7 @@
 
      $scope.saveClassified = function(classified){
         if (classified){
+          classified.contact = contact;
           $scope.classifieds.push(classified);
           $scope.classified = {}; 
           $scope.closeSidebar();
